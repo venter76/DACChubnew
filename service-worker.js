742298530f7711410075be4e600172ce6b5b02c3
Stephen <venter76@gmail.com>
2024-05-26 11:46:54 +0200

    2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v3';
+const CACHE_NAME = 'static-cache-v4';
 const STATIC_ASSETS = [
     '/',  // Assuming '/home' serves home.ejs
     '/login', // Assuming '/login' serves login.ejs
